c726772a959debd73dd161f24f9e9d84e128042c
Ninh Bình <ninhhbinhh@gmail.com>
2023-11-08 08:45:18 +0700

    base_frameWork

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 session_start();
 require_once 'env.php';
-require_once 'vendor/autoload.php';
+require_once 'modules/autoload.php';
 require_once 'router/route.php';
--- modules/autoload.php
+++ modules/autoload.php
@@ -1,6 +1,6 @@
 <?php
 require_once './config/App.php';
-require_once './vendor/autoload/my_autoload.php';
+require_once './modules/autoload/my_autoload.php';
 $config = require('config/config.php');
 App::setConfig($config);
 
